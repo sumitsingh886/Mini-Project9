4e946fd20ad1b1f33215f3264ec4a2ee4cdfc1c4
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-10-16 14:16:49 +0530

    add  contact us page with li tag using anchor tag

--- Online shoping system/index.php
+++ Online shoping system/index.php
@@ -29,6 +29,7 @@
             <li><a href="all_products.php">All Products</a></li>
             <li><a href="my_account.php">My Account</a></li>
             <li><a href="cart.php">Shopping Cart</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
         </ul>
     </div>
     </div>
