79eca63073945c10ad987716302dd9a68a5d9656
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-10-16 11:15:10 +0530

    link with stylesheet and google font in head tag

--- Online shoping system/index.php
+++ Online shoping system/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Online Shop</title>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style.css">
     <link href='https://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet'>
 </head>
 <body>
