48ab03cb247f88543e6c413890cd21d3aefed256
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-10-16 13:47:08 +0530

    add with table tag in header class and using table attribute

--- Online shoping system/index.php
+++ Online shoping system/index.php
@@ -9,8 +9,13 @@
 <body>
 <div class="main_wrapper">
     <div class="header">
+        <table border="0" width="100%" bgcolor="#ffb6c1">
+            <tr>
+                <td align="right"><h4 style="font-family:'Arizonia',serif;font-size:30px; margin:0;" ;>Online Shop</h4></td>
+            </tr>
+        </table>
 
-    </div>div>
+    </div>
 
     </div>
 </body>
