d971d6c3b6cbc7266026373c0e3c354ce7382803
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-10-16 13:53:32 +0530

    add search box and search button using td tag

--- Online shoping system/index.php
+++ Online shoping system/index.php
@@ -12,6 +12,10 @@
         <table border="0" width="100%" bgcolor="#ffb6c1">
             <tr>
                 <td align="right"><h4 style="font-family:'Arizonia',serif;font-size:30px; margin:0;" ;>Online Shop</h4></td>
+                <td><label>
+                        <input type="text" placeholder="Search for products, brand and more" name="user_query" style="width:550px; height:35px;">
+                    </label>
+                    <input type="button" name="search" value="Search" style="width:60px;height:35px;"></td>
             </tr>
         </table>
 
