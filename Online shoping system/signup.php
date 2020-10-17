5c2fe7e2ab900741e55481cf434d7f3d1e9db54d
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-10-17 23:06:14 +0530

    add link in html tag for sign up page

---
+++ Online shoping system/signup.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Online Shop</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Arizonia' rel='stylesheet'>
+    <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css">
+</head>
+<body>
+</body>
+</html>
