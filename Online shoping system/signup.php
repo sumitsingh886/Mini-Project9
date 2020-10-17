ad159bf61de7d3b850ed30293554db01ed1fe95e
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-10-17 23:24:22 +0530

    using bootstrap class form group for create a password box

--- Online shoping system/signup.php
+++ Online shoping system/signup.php
@@ -36,6 +36,9 @@
                     <div class="form-group">
                         <input type="email" class="form-control" name="email" placeholder="Email" required="true" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
                     </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="password" placeholder="Password(min. 6 characters)" required="true" pattern=".{6,}">
+                    </div>
                 </form>
             </div>
         </div>
