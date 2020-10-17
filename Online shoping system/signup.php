b9f7f7b6d284ad447850a7af17b5e5f57be3ac9f
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-10-17 23:17:33 +0530

    using heading tag for sign up

--- Online shoping system/signup.php
+++ Online shoping system/signup.php
@@ -28,6 +28,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-lg-offset-4">
+                <h1><b>SIGN UP</b></h1>
             </div>
         </div>
     </div>
