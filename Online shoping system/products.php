f5d1eb17b705cd428120171be4586d5e3042a205
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 22:14:57 +0530

    add nineth image in product page and execute if else block statement and using heading tags and paragraph tags and using echo functions

--- Online shoping system/products.php
+++ Online shoping system/products.php
@@ -251,6 +251,34 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <a href="cart.php">
+                        <img src="image/raymond.jpg" alt="Raymond shirt" width="100%" height="100%">
+                    </a>
+                    <center>
+                        <div class="caption">
+                            <h3>Raymond</h3>
+                            <p>Price: Rs. 1500.00</p>
+                            <?php if(!isset($_SESSION['email'])){  ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            }
+                            else{
+                                if(check_if_added_to_cart(9)){
+                                    echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                }else{
+                                    ?>
+                                    <a href="cart_add.php?id=9" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </center>
+                </div>
+            </div>
 
 </div>
 </body>
