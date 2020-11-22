426bdce0660353289b6a5605efc65551ea1b08ab
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 22:08:28 +0530

    add fifth image in product page and execute if else block statement

--- Online shoping system/products.php
+++ Online shoping system/products.php
@@ -141,6 +141,34 @@
                 </div>
             </div>
     </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <a href="cart.php">
+                        <img src="image/titan301.jpg" alt="Titan 301" width="100%" height="100%">
+                    </a>
+                    <center>
+                        <div class="caption">
+                            <h3>Titan Model #301</h3>
+                            <p>Price: Rs. 13000.00</p>
+                            <?php if(!isset($_SESSION['email'])){  ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            }
+                            else{
+                                if(check_if_added_to_cart(5)){
+                                    echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                }else{
+                                    ?>
+                                    <a href="cart_add.php?id=5" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </center>
+                </div>
+            </div>
 
 </div>
 </body>
