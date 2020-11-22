7265cf3ad74aa4e6337635d86135b20c2f8f8832
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 22:12:43 +0530

    add eight image in product page and execute if else block statement and using heading tags and paragraph tags and using echo functions

--- Online shoping system/products.php
+++ Online shoping system/products.php
@@ -223,6 +223,34 @@
                     </center>
                 </div>
             </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <a href="cart.php">
+                        <img src="image/favreleuba.jpg" alt="Favre Leuba" width="100%" height="100%">
+                    </a>
+                    <center>
+                        <div class="caption">
+                            <h3>Favre Leuba #111</h3>
+                            <p>Price: Rs. 18000.00</p>
+                            <?php if(!isset($_SESSION['email'])){  ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            }
+                            else{
+                                if(check_if_added_to_cart(8)){
+                                    echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                }else{
+                                    ?>
+                                    <a href="cart_add.php?id=8" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </center>
+                </div>
+            </div>
+        </div>
 
 </div>
 </body>
