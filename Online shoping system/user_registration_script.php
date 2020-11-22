84e3db695af84faaf29180f6612370cd51811df2
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 12:08:48 +0530

    if condition and script run

--- Online shoping system/user_registration_script.php
+++ Online shoping system/user_registration_script.php
@@ -23,4 +23,13 @@ $address=mysqli_real_escape_string($con,$_POST['address']);
 $duplicate_user_query="select id from user where email='$email'";
 $duplicate_user_result=mysqli_query($con,$duplicate_user_query) or die(mysqli_error($con));
 $rows_fetched=mysqli_num_rows($duplicate_user_result);
+if($rows_fetched>0){
+     //duplicate registration
+    //header('location: signup.php');
+?>
+<script>
+    window.alert("Email already exists in our database!");
+</script>
+<meta http-equiv="refresh" content="1;url=signup.php" />
+
 
