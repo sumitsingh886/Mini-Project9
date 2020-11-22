1bf00151fab06942ee21ec55261087eb3a87d30e
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 11:58:31 +0530

    link with signup page

--- Online shoping system/user_registration_script.php
+++ Online shoping system/user_registration_script.php
@@ -6,3 +6,5 @@ $email=mysqli_real_escape_string($con,$_POST['email']);
 $regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
 if(!preg_match($regex_email,$email)){
     echo "Incorrect email. Redirecting you back to registration page...";
+    ?>
+    <meta http-equiv="refresh" content="2;url=signup.php" />
