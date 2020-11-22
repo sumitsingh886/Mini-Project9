f949318db6f06ef9b04f7e68da061690fa711bde
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 12:32:40 +0530

    if block statement statement execute

--- Online shoping system/login_submit.php
+++ Online shoping system/login_submit.php
@@ -10,3 +10,9 @@ echo "Incorrect email. Redirecting you back to login page...";
     <?php
 }
 $password=md5(md5(mysqli_real_escape_string($con,$_POST['password'])));
+if(strlen($password)<6){
+    echo "Password should have atleast 6 characters. Redirecting you back to login page...";
+    ?>
+    <meta http-equiv="refresh" content="2;url=login.php" />
+    <?php
+}
