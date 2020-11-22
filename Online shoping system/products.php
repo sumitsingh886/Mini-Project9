594c8dbd562df670ff6319154f7af4f8e3928d59
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 22:16:17 +0530

    add tenth image in product page and execute if else block statement and using heading tags and paragraph tags and using echo functions

--- Online shoping system/products.php
+++ Online shoping system/products.php
@@ -279,6 +279,33 @@
                     </center>
                 </div>
             </div>
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnail">
+                    <a href="cart.php">
+                        <img src="image/charles.jpg" alt="Charles shirt" width="100%" height="100%">
+                    </a>
+                    <center>
+                        <div class="caption">
+                            <h3>Charles</h3>
+                            <p>Price: Rs. 1000.00</p>
+                            <?php if(!isset($_SESSION['email'])){  ?>
+                                <p><a href="login.php" role="button" class="btn btn-primary btn-block">Buy Now</a></p>
+                                <?php
+                            }
+                            else{
+                                if(check_if_added_to_cart(10)){
+                                    echo '<a href="#" class=btn btn-block btn-success disabled>Added to cart</a>';
+                                }else{
+                                    ?>
+                                    <a href="cart_add.php?id=10" class="btn btn-block btn-primary " name="add" value="add" class="btn btn-block btr-primary">Add to cart</a>
+                                    <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </center>
+                </div>
+            </div>
 
 </div>
 </body>
