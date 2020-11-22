bd0cb51cca02ec612eb971ed996e84195f43a64f
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 12:38:32 +0530

    if and else block statement execute

--- Online shoping system/login_submit.php
+++ Online shoping system/login_submit.php
@@ -27,3 +27,14 @@ if($rows_fetched==0){
     window.alert("Wrong username or password");
 </script>
 <meta http-equiv="refresh" content="1;url=login.php" />
+    <?php
+    //header('location: login');
+    //echo "Wrong email or password.";
+}else{
+    $row=mysqli_fetch_array($user_authentication_result);
+    $_SESSION['email']=$email;
+    $_SESSION['id']=$row['id'];  //user id
+    header('location: products.php');
+}
+
+?>
