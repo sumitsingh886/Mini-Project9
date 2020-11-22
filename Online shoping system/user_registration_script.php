2b93590c14759503ef89de7eea3833d3b8c4dbc6
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 11:56:17 +0530

    regex using in email and if condition execute

--- Online shoping system/user_registration_script.php
+++ Online shoping system/user_registration_script.php
@@ -3,4 +3,6 @@ require 'connection.php';
 session_start();
 $name= mysqli_real_escape_string($con,$_POST['name']);
 $email=mysqli_real_escape_string($con,$_POST['email']);
-?>
+$regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
+if(!preg_match($regex_email,$email)){
+    echo "Incorrect email. Redirecting you back to registration page...";
