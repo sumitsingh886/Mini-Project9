3a97c6c27ded9813614fb2004f377a28978522ec
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 12:26:33 +0530

    using email variable in sql function real escape string

--- Online shoping system/login_submit.php
+++ Online shoping system/login_submit.php
@@ -1,3 +1,5 @@
 <?php
 require 'connection.php';
 session_start();
+$email=mysqli_real_escape_string($con,$_POST['email']);
+$regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
