bb22686fe00a725e63e82489275c50457e02755a
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 12:00:46 +0530

    set password length using conditional statement

--- Online shoping system/user_registration_script.php
+++ Online shoping system/user_registration_script.php
@@ -8,3 +8,10 @@ if(!preg_match($regex_email,$email)){
     echo "Incorrect email. Redirecting you back to registration page...";
     ?>
     <meta http-equiv="refresh" content="2;url=signup.php" />
+    <?php
+}
+$password=md5(md5(mysqli_real_escape_string($con,$_POST['password'])));
+if(strlen($password)<6){
+echo "Password should have atleast 6 characters. Redirecting you back to registration page...";
+?>
+
