a3cbc6819028a6eb930656348ab5835ce803647a
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-22 12:35:59 +0530

    rows_fetched condition in if condition

--- Online shoping system/login_submit.php
+++ Online shoping system/login_submit.php
@@ -19,3 +19,7 @@ if(strlen($password)<6){
 $user_authentication_query="select id,email from user where email='$email' and password='$password'";
 $user_authentication_result=mysqli_query($con,$user_authentication_query) or die(mysqli_error($con));
 $rows_fetched=mysqli_num_rows($user_authentication_result);
+if($rows_fetched==0){
+    //no user
+    //redirecting to same login page
+    ?>
