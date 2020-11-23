7e248e8c5520bec33ceef340d75fca3caf50f3d4
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-23 22:03:35 +0530

    add header menu using table tag and attribute

--- Online shoping system/cart.php
+++ Online shoping system/cart.php
@@ -9,6 +9,19 @@
 <body>
 <div class="main_wrapper">
     <div class="header">
+        <table border="0" width="100%" bgcolor="#ffb6c1">
+            <tr>
+                <td align="right"><h4 style="font-family:'Arizonia',serif;font-size:30px; margin:0;" ;>Online Shop</h4></td>
+                <td><label>
+                        <input type="text" placeholder="Search for products, brand and more" name="user_query" style="width:550px; height:35px;">
+                    </label>
+                    <input type="button" name="search" value="Search" style="width:60px;height:35px;"></td>
+                <th><a href="login.php" style="text-decoration:none;"><font color="black">Login</font></a></th>
+                <th><a href="signup.php" style="text-decoration:none;"><font color="black">Sign up</font></a></th>
+                <th><a href="#" style="text-decoration:none;"><font color="black">More</font></a></th>
+                <th><a href="cart.php" style="text-decoration:none;"><font color="black">Cart</font></a></th>
+            </tr>
+        </table>
     </div>
 </div>
 </body>
