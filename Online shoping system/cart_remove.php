057806e6cffb68e0935b918f870198d4bcb8c228
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-24 11:02:53 +0530

    initialise add_to_cart_query variable

--- Online shoping system/cart_remove.php
+++ Online shoping system/cart_remove.php
@@ -3,3 +3,5 @@ require 'connection.php';
 //require 'header.php';
 $item_id=$_GET['id'];
 $user_id=$_SESSION['id'];
+$add_to_cart_query="insert into user_item(user_id,item_id,status) values ('$user_id','$item_id','Added to cart')";
+$add_to_cart_result=mysqli_query($con,$add_to_cart_query) or die(mysqli_error($con));
