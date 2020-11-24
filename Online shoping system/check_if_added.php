096c21eb8120c4fbfb9a83fcc9b22d59ab326cfc
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-24 11:25:25 +0530

    execute if block statement

--- Online shoping system/check_if_added.php
+++ Online shoping system/check_if_added.php
@@ -5,5 +5,7 @@ function check_if_added_to_cart($item_id){
     $product_check_query="select * from user_item where item_id='$item_id' and user_id='$user_id' and status='Added to cart'";
     $product_check_result=mysqli_query($con,$product_check_query) or die(mysqli_error($con));
     $num_rows=mysqli_num_rows($product_check_result);
+    if($num_rows>=1)return 1;
+    return 0;
 
 }
