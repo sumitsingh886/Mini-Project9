fe2eb19d0e452589680df6a2f5323527b9791f1d
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-24 11:56:47 +0530

    set link for again login page after logout

--- logout.php
+++ logout.php
@@ -35,6 +35,19 @@
             </tr>
         </table>
     </div>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading"></div>
+                    <div class="panel-body">
+                        <p>You have been logged out. <a href="login.php">Login again.</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </body>
 </html>
