92d59d53a20238933af0d02e7cf43f1f4d5c0878
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-24 10:42:41 +0530

    initialise add_to_cart_query variable

--- Online shoping system/cart_add.php
+++ Online shoping system/cart_add.php
@@ -3,3 +3,4 @@ require 'connection.php';
 session_start();
 $item_id=$_GET['id'];
 $user_id=$_SESSION['id'];
+$add_to_cart_query="insert into user_item(user_id,item_id,status) values ('$user_id','$item_id','Added to cart')";
