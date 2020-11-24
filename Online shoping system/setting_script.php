18c406d8588a35a2758a45666a0fad775773f0b4
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-24 17:05:23 +0530

    run queries

--- Online shoping system/setting_script.php
+++ Online shoping system/setting_script.php
@@ -4,3 +4,6 @@ require 'connection.php';
 if(!isset($_SESSION['email'])){
     header('location:index.php');
 }
+$old_password= md5(md5(mysqli_real_escape_string($con,$_POST['oldPassword'])));
+$new_password= md5(md5(mysqli_real_escape_string($con,$_POST['newPassword'])));
+$email=$_SESSION['email'];
