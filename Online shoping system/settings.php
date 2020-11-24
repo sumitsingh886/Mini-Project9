4f552e3924245bed2abd4b8c7cd01b057f8169fc
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-24 16:50:42 +0530

    create field for submit button

--- Online shoping system/settings.php
+++ Online shoping system/settings.php
@@ -50,6 +50,9 @@
                     <div class="form-group">
                         <input type="password" class="form-control" name="retype" placeholder="Re-type new password">
                     </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Change">
+                    </div>
                 </form>
             </div>
         </div>
