e33d72f4b6a78f9ad35cddabcfc42c79b6c1bb54
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-24 16:47:22 +0530

    create field for old password

--- Online shoping system/settings.php
+++ Online shoping system/settings.php
@@ -41,7 +41,9 @@
             <div class="col-xs-4 col-xs-offset-4">
                 <h1>Change Password</h1>
                 <form method="post" action="setting_script.php">
-
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="oldPassword" placeholder="Old Password">
+                    </div>
                 </form>
             </div>
         </div>
