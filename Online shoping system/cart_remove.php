03445d7526e1e29d42ef6a58bb6d684286b6d62e
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-11-25 11:11:17 +0530

    some changement for sql queries

--- Online shoping system/cart_remove.php
+++ Online shoping system/cart_remove.php
@@ -3,7 +3,7 @@ require 'connection.php';
 //require 'header.php';
 $item_id=$_GET['id'];
 $user_id=$_SESSION['id'];
-$add_to_cart_query="insert into user_item(user_id,item_id,status) values ('$user_id','$item_id','Added to cart')";
-$add_to_cart_result=mysqli_query($con,$add_to_cart_query) or die(mysqli_error($con));
-header('location: products.php');
+$delete_query="delete from user_item where user_id='$user_id' and item_id='$item_id'";
+$delete_query_result=mysqli_query($con,$delete_query) or die(mysqli_error($con));
+header('location: cart.php');
 ?>
